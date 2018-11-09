17dd876bba5340b40ba29d4e28047eec69d470d9
Dapper 02 <angelfcancun@gmail.com>
2018-11-09 12:44:11 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrerComponent } from './registrer/registrer.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { PrivadoComponent } from './privado/privado.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegistrerComponent,
+    LoginpageComponent,
+    PrivadoComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
